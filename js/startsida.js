1c42121527ad789b042718ee2c5378c1ccc00e55
karrelo <kardo.rahim@hotmail.se>
2018-09-21 11:48:52 +0200

    kommit

---
+++ js/startsida.js
@@ -0,0 +1,11 @@
+/*$("#bildspelMain > div:gt(0)").hide();
+
+setInterval(function() {
+    $('#bildspelMain > div:first')
+      .fadeOut(1000)
+      .next()
+      .fadeIn(1000)
+      .end()
+      .appendTo('#bildspelMain');
+  }, 3000);
+*/

78f1d09a8ed38546c57bf1935e6e241dbf765a9f
karrelo <34681537+karrelo@users.noreply.github.com>
2018-10-04 15:46:53 +0200

    Add files via upload

---
+++ Grupp_31/js/kontakt.js
@@ -0,0 +1,172 @@
+// fyller textfälten med infon som fyllts i tidigare. 
+
+function displayCred() {     
+    var a = localStorage.getItem(1);
+    var b = localStorage.getItem(2);
+    var c = localStorage.getItem(3);
+    var d = localStorage.getItem(4);
+    
+    
+
+var a1 = document.getElementById("namnFalt").value
+var b1 = document.getElementById("mailFalt").value
+var c1 = document.getElementById("telFalt").value
+var d1 = document.getElementById("medFalt").value
+
+if (a1 === "") {
+    document.getElementById("namnFalt").value = a;
+    document.getElementById("mailFalt").value = b;
+    document.getElementById("telFalt").value = c;
+    document.getElementById("medFalt").value = d;
+
+    rensaAllt();
+
+
+}
+}
+
+// metod för att se om namnet är mindre än tre karaktärer vilket skapar röd text.
+function checkNamn() {
+    var strText = document.getElementById("namnFalt").value;
+    var strText2 = document.getElementById("namnFalt");
+    
+    if(strText.length < 2) {
+     strText2.style.color = "red";
+    } else {
+     strText2.style.color = "black";
+    }
+}
+// hämtar input från olika fälten i kontaktformuläret och placerar dom i Local Storage, samt rensar fälten.
+function saveInput() {
+
+   // hämtar input och lagrar i variabler. 
+
+    var namnFalt = document.getElementById("namnFalt").value;
+    var mailFalt = document.getElementById("mailFalt").value;
+    var telFalt = document.getElementById("telFalt").value;
+    var medFalt = document.getElementById("medFalt").value;
+    // placerar datan i Local Storage.
+    localStorage.setItem("1", namnFalt);
+    localStorage.setItem("2", mailFalt);
+    localStorage.setItem("3", telFalt);
+    localStorage.setItem ("4", medFalt);
+    // rensar fälten.
+    document.getElementById("namnFalt").value = "";
+    document.getElementById("mailFalt").value = "";
+    document.getElementById("telFalt").value = "";
+    document.getElementById("medFalt").value = "";
+}
+
+// rensar Local Storage.
+
+function rensaAllt() {
+    localStorage.clear();
+}
+
+// metod för att säkerställa att man inte skickar in med tom namnfält.
+
+function kollaNamn() {
+    var a2 = document.getElementById("namnFalt").value
+    
+    if (a2 ==="") {
+        
+    } else {
+        kollaMail();
+        
+    }
+
+}
+// metod för att säkerställa att man inte skickar in med tom mailfält.
+function kollaMail() {
+    var b2 = document.getElementById("mailFalt").value
+
+    if(b2 ==="") {
+        
+    } else {
+        kollaTel();
+    }
+
+}
+// metod för att säkerställa att man inte skickar in med tomt telefonfält.
+
+function kollaTel() {
+    var c2 = document.getElementById("telFalt").value
+
+    if(c2==="") {
+        
+    } else {
+        kollaMed();
+    }
+
+}
+
+// metod för att säkerställa att man inte skickar in med tomt meddelandefält.
+// om allt är OK skickar den formuläret samt anropar animeringen.
+
+function kollaMed() {
+    var d2 = document.getElementById("medFalt").value
+
+    if(d2==="") {
+        
+    } else {
+        saveInput();
+        flyttaVanster();
+        fadeaUt();
+        nyaIn();
+        visaText();
+
+      
+    }
+}
+
+// animerar handen att gå vänster.
+
+function flyttaVanster() {
+ 
+    
+    $("#handen").animate({
+        left: '+=40px'
+        
+        
+    },400,  );
+    
+}
+
+// Animerar bort handen.
+
+function fadeaUt() {
+    $("#handen").fadeOut("fast");        
+}
+
+// animerar fram nya bilden.
+
+function nyaIn() {
+    $("#tummeUp").fadeIn("slow");
+
+}
+
+
+// fäster en text på bilden samt gör den synlig.
+
+function visaText() {
+    var tumme = document.getElementById("tummeUp");
+    var skickat = document.getElementById("sentText")
+
+    tumme.appendChild(skickat);
+
+    $("#sentText").fadeIn("slow");
+
+    
+}
+
+// KARTAN 
+
+function initMap() {
+       
+    var uluru = {lat: 59.2552, lng: 15.2482};
+
+ var map = new google.maps.Map(
+ document.getElementById('map'), {zoom: 15, center: uluru});
+
+var marker = new google.maps.Marker({position: uluru, map: map});
+}
---
+++ Grupp_31/js/portfolio.js
@@ -0,0 +1,35 @@
+
+
+$(document).ready(()=>{
+  //Vid tryck på knappen moreBtn hämtar idt på tryckt knapp
+  $('.moreBtn').click(function() {
+      var myBtn = $(this).attr("id");
+
+      //Läser in vilken knapp det är som är vald för följande variabler
+      var word= 'Hello';
+      console.log(word.length);
+      console.log(word[0]);
+      console.log(word[4]);
+      var index = myBtn.substr(myBtn.length -1);
+      var dots = "dots"+index;
+      var more = "more"+index;
+
+      //Hämtar objektet för indexet
+      var dots = document.getElementById(dots);
+      var moreText = document.getElementById(more);
+      var btnText = document.getElementById(myBtn);
+    
+    //If sats som hanterar läs-knapparna
+      if (dots.style.display === "none") {
+        dots.style.display = "block";
+        btnText.innerHTML = "Läs mer"; 
+        moreText.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Läs mindre"; 
+        moreText.style.display = "inline";
+      }
+  });
+});
+
+
